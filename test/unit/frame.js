1756070a4eb2fee012acf753098e23346257f22b
Glen E. Ivey <gleneivey@wontology.org>
2009-07-23 07:51:27 -0700

    Added test cases for (i)frame-in-(i)frame nesting, no new bugs found. Testing currently goes 7 levels of nesting deep, but can be adjusted by constant assigned to 'endingDepth' in test/unit/frame.js.

--- test/unit/frame.js
+++ test/unit/frame.js
@@ -10,8 +10,16 @@
 module("frame");
 
 
+// In general, the tests here use the files ../html/iframe*.html
+// I've tried to keep all of the id attributes and text fragments in these
+//   files unique from each other and from the content of index.html
+//   to ensure that we can correctly identify which file has loaded into
+//   which frame/window.  When making modifications/additions, paying
+//   attention to uniqueness is recommended.
+
+
 // all tests to next comment rely on content of ../html/iframe1.html and
-//                                              ../html/iframe1a.html and
+//                                              ../html/iframe1a.html
 
     // iframe1 and iframe1a are identical in structure (so we can use the
     //   same assertions against both), but different in content text (so
@@ -73,7 +81,7 @@ test("Global scope for JS code in an iframe refers to that iframe's window/docum
     expect(2);
 
     var idoc = document.getElementById('loadediframe').contentDocument;
-    mtch = idoc.getElementById('js_generated_p').innerHTML.match(/Dynamic/);
+    var mtch = idoc.getElementById('js_generated_p').innerHTML.match(/Dynamic/);
     try{ ok(mtch && mtch.length > 0,
         "Can get content from dynamically-generate p element");
     }catch(e){print(e);}
@@ -106,6 +114,81 @@ test("IFRAMEs reload with accessible content", function() {
 });
 
 
+// this test relies on iframe3.html, iframe2.html, and iframeN.html
+test("IFRAMEs can be nested, created dynamically", function() {
+    var startingDepth = 3;
+    var endingDepth   = 7
+    expect(5 + (3*((endingDepth - startingDepth)+1)));
+
+    // manually load iframe3.html
+    var firstIframe = document.getElementById('emptyiframe');
+    firstIframe.src = "html/iframe3.html";
+
+    // iframe3.html contains a static <iframe> element that loads iframe2.html,
+    // so at this point, we should have both loaded, with the structure that
+    //     index.html --contains--> iframe3.html --contains--> iframe2.html
+    // w/ id's =      emptyiframe               nested1Level
+
+
+    // first, verify that we've got the structure we expect:
+    var mtch = firstIframe.contentDocument.title.match(/nested-IFRAME/);
+    try{ok (mtch && mtch.length > 0,
+        "top-level IFRAME reloaded from correct source");
+    }catch(e){print(e);}
+
+    var secondIframe = firstIframe.contentDocument.
+      getElementById('nested1Level');
+    mtch = secondIframe.contentDocument.title.match(/IFRAME loading/);
+    try{ok (mtch && mtch.length > 0,
+        "can access content of an IFRAME nested in another");
+    }catch(e){print(e);}
+
+    try{ok (secondIframe.contentDocument.parentWindow.parent.parent == window,
+        "can follow path from nested IFRAME to root window");
+    }catch(e){print(e);}
+
+    try{ok (secondIframe.contentWindow.parent.parent == window,
+        "also path through .contentWindow to root window");
+    }catch(e){print(e);}
+
+    try{ok (secondIframe.contentWindow.top == window,
+        "nested IFRAME has correct .top");
+    }catch(e){print(e);}
+
+
+    // OK, now we'll programatically extend the nesting depth from 2 to many
+    for (var c = startingDepth, bottomIframe = secondIframe; c <= endingDepth;
+         c++){
+
+        // add a new iframe within the current leaf iframe
+        var newIframe = bottomIframe.contentDocument.createElement("iframe");
+        newIframe.setAttribute("id", "iframe_of_depth_" + c);
+        var bottomBody = bottomIframe.contentDocument.
+          getElementsByTagName('body')[0];
+        bottomBody.appendChild(newIframe);
+        newIframe.src = "html/iframeN.html";
+        bottomIframe = newIframe;
+
+        // verify contents of just-leaded iframe
+        mtch = bottomIframe.contentDocument.getElementById('nestingLevel').
+          innerHTML.match(/[0-9]+/);
+        try{ok (mtch && mtch.length > 0 && parseInt(mtch[0]) == c,
+            "nested " + c + " levels: can access content");
+        }catch(e){print(e);}
+
+        for (var aWindow = bottomIframe.contentWindow, cn = c; cn > 0; cn--)
+            aWindow = aWindow.parent;
+        try{ok (aWindow == window,
+            "nested " + c + " levels: can follow path to root window");
+        }catch(e){print(e);}
+
+        try{ok (bottomIframe.contentWindow.top == window,
+            "nested " + c + " levels: IFRAME has correct .top");
+        }catch(e){print(e);}
+    }
+});
+
+
 // all tests to next comment rely on content of ../html/iframe2.html
 test("IFRAMEs reload on assignment to 'src'", function() {
     expect(2);
@@ -117,7 +200,7 @@ test("IFRAMEs reload on assignment to 'src'", function() {
     }catch(e){print(e);}
 
     var para = iframe.contentDocument.getElementById('aParaInAnIframe');
-    mtch = para.innerHTML.match(/short paragraph/);
+    var mtch = para.innerHTML.match(/short paragraph/);
     try{ok (mtch && mtch.length > 0,
         "IFRAME reloaded from correct source");
     }catch(e){print(e);}
