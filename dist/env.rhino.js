cee6d5af22fb25285aac0ed0bfffe91ec67cd142
Glen E. Ivey <gleneivey@wontology.org>
2009-07-24 15:49:56 -0700

    Ooops, <body> and <iframe> didn't work when onload assigned as HTML attribute but fixed now.

--- dist/env.rhino.js
+++ dist/env.rhino.js
@@ -5302,18 +5302,18 @@ HTMLDocument.prototype = new DOMDocument;
 __extend__(HTMLDocument.prototype, {
     createElement: function(tagName){
           // throw Exception if the tagName string contains an illegal character
-          if (__ownerDocument__(this).implementation.errorChecking && 
+          if (__ownerDocument__(this).implementation.errorChecking &&
                 (!__isValidName__(tagName))) {
               throw(new DOMException(DOMException.INVALID_CHARACTER_ERR));
           }
           tagName = tagName.toUpperCase();
           // create DOMElement specifying 'this' as ownerDocument
-          //This is an html document so we need to use explicit interfaces per the 
+          //This is an html document so we need to use explicit interfaces per the
           if(     tagName.match(/^A$/))                 {node = new HTMLAnchorElement(this);}
           else if(tagName.match(/AREA/))                {node = new HTMLAreaElement(this);}
           else if(tagName.match(/BASE/))                {node = new HTMLBaseElement(this);}
           else if(tagName.match(/BLOCKQUOTE|Q/))        {node = new HTMLQuoteElement(this);}
-          else if(tagName.match(/BODY/))                {node = new HTMLElement(this);}
+          else if(tagName.match(/BODY/))                {node = new HTMLBodyElement(this);}
           else if(tagName.match(/BR/))                  {node = new HTMLElement(this);}
           else if(tagName.match(/BUTTON/))              {node = new HTMLButtonElement(this);}
           else if(tagName.match(/CAPTION/))             {node = new HTMLElement(this);}
@@ -5358,27 +5358,27 @@ __extend__(HTMLDocument.prototype, {
           else{
             node = new HTMLElement(this);
           }
-        
+
           // assign values to properties (and aliases)
           node.tagName  = tagName;
           return node;
     },
     get anchors(){
         return new HTMLCollection(this.getElementsByTagName('a'), 'Anchor');
-        
+
     },
     get applets(){
         return new HTMLCollection(this.getElementsByTagName('applet'), 'Applet');
-        
+
     },
-    get body(){ 
+    get body(){
         var nodelist = this.getElementsByTagName('body');
         return nodelist.item(0);
-        
+
     },
     set body(html){
         return this.replaceNode(this.body,html);
-        
+
     },
 
     get title(){
@@ -5406,44 +5406,44 @@ __extend__(HTMLDocument.prototype, {
     //set/get cookie see cookie.js
     get domain(){
         return this._domain||window.location.domain;
-        
+
     },
     set domain(){
-        /* TODO - requires a bit of thought to enforce domain restrictions */ 
-        return; 
-        
+        /* TODO - requires a bit of thought to enforce domain restrictions */
+        return;
+
     },
     get forms(){
       return new HTMLCollection(this.getElementsByTagName('form'), 'Form');
     },
     get images(){
         return new HTMLCollection(this.getElementsByTagName('img'), 'Image');
-        
+
     },
-    get lastModified(){ 
+    get lastModified(){
         /* TODO */
-        return this._lastModified; 
-    
+        return this._lastModified;
+
     },
     get links(){
         return new HTMLCollection(this.getElementsByTagName('a'), 'Link');
-        
+
     },
     get location(){
         return $w.location
     },
     get referrer(){
         /* TODO */
-        return this._refferer; 
-        
+        return this._refferer;
+
     },
     get URL(){
         /* TODO*/
-        return this._url; 
-        
+        return this._url;
+
     },
-	close : function(){ 
-	    /* TODO */ 
+	close : function(){
+	    /* TODO */
 	    this._open = false;
     },
 	getElementsByName : function(name){
@@ -5460,28 +5460,28 @@ __extend__(HTMLDocument.prototype, {
         }
         return retNodes;
 	},
-	open : function(){ 
+	open : function(){
 	    /* TODO */
-	    this._open = true;  
+	    this._open = true;
     },
-	write: function(htmlstring){ 
+	write: function(htmlstring){
 	    /* TODO */
-	    return; 
-	
+	    return;
+
     },
-	writeln: function(htmlstring){ 
-	    this.write(htmlstring+'\n'); 
+	writeln: function(htmlstring){
+	    this.write(htmlstring+'\n');
     },
-	toString: function(){ 
-	    return "Document" +  (typeof this._url == "string" ? ": " + this._url : ""); 
+	toString: function(){
+	    return "Document" +  (typeof this._url == "string" ? ": " + this._url : "");
     },
-	get innerHTML(){ 
-	    return this.documentElement.outerHTML; 
-	    
+	get innerHTML(){
+	    return this.documentElement.outerHTML;
+
     },
 	get __html__(){
 	    return true;
-	    
+
     }
 });
 
@@ -6086,7 +6086,23 @@ __extend__(HTMLQuoteElement.prototype, {
     }
 });
 
-$w.HTMLQuoteElement = HTMLQuoteElement;		$debug("Defining HTMLButtonElement");
+$w.HTMLQuoteElement = HTMLQuoteElement;		$debug("Defining HTMLBodyElement");
+/*
+* HTMLBodyElement - DOM Level 2
+*/
+var HTMLBodyElement = function(ownerDocument) {
+    this.HTMLElement = HTMLElement;
+    this.HTMLElement(ownerDocument);
+};
+HTMLBodyElement.prototype = new HTMLElement;
+__extend__(HTMLBodyElement.prototype, {
+    onload: function(event){
+        __eval__(this.getAttribute('onload')||'')
+    }
+});
+
+$w.HTMLBodyElement = HTMLBodyElement;
+$debug("Defining HTMLButtonElement");
 /* 
 * HTMLButtonElement - DOM Level 2
 */
@@ -6375,6 +6391,9 @@ __extend__(HTMLFrameElement.prototype, {
             }
         }
         return this._content;
+    },
+    onload: function(event){
+        __eval__(this.getAttribute('onload')||'')
     }
 });
 
@@ -7126,7 +7145,6 @@ __extend__(HTMLScriptElement.prototype, {
     onload: function(event){
         __eval__(this.getAttribute('onload')||'')
     }
-
 });
 
 $w.HTMLScriptElement = HTMLScriptElement;$debug("Defining HTMLSelectElement");
--- src/html/frame.js
+++ src/html/frame.js
@@ -76,6 +76,9 @@ __extend__(HTMLFrameElement.prototype, {
             }
         }
         return this._content;
+    },
+    onload: function(event){
+        __eval__(this.getAttribute('onload')||'')
     }
 });
 
