b3310588d4ac28088559c419d13bfbf310ccdc5d
Glen E. Ivey <gleneivey@wontology.org>
2009-08-11 20:21:54 -0700

    Implementing the <textarea> element's properties and methods.

--- src/html/textarea.js
+++ src/html/textarea.js
@@ -1,5 +1,5 @@
 $debug("Defining HTMLTextAreaElement");
-/* 
+/*
 * HTMLTextAreaElement - DOM Level 2
 */
 var HTMLTextAreaElement = function(ownerDocument) {
@@ -8,7 +8,104 @@ var HTMLTextAreaElement = function(ownerDocument) {
 };
 HTMLTextAreaElement.prototype = new HTMLElement;
 __extend__(HTMLTextAreaElement.prototype, {
-    //TODO
+    get cols(){
+        return this.getAttribute('cols');
+    },
+    set cols(value){
+        this.setAttribute('cols', value);
+    },
+    get rows(){
+        return this.getAttribute('rows');
+    },
+    set rows(value){
+        this.setAttribute('rows', value);
+    },
+
+    get defaultValue(){
+        return this.getAttribute('defaultValue');
+    },
+    set defaultValue(value){
+        this.setAttribute('defaultValue', value);
+    },
+    get form(){
+        var parent = this.parent;
+        while(parent.nodeName.toLowerCase() != 'form'){
+            parent = parent.parent;
+        }
+        return parent;
+    },
+    get accessKey(){
+        return this.getAttribute('accesskey');
+    },
+    set accessKey(value){
+        this.setAttribute('accesskey',value);
+    },
+    get access(){
+        return this.getAttribute('access');
+    },
+    set access(value){
+        this.setAttribute('access', value);
+    },
+    get disabled(){
+        return (this.getAttribute('disabled')=='disabled');
+    },
+    set disabled(value){
+        this.setAttribute('disabled', (value ? 'disabled' :''));
+    },
+    get name(){
+        return this.getAttribute('name')||'';
+    },
+    set name(value){
+        this.setAttribute('name', value);
+    },
+    get readOnly(){
+        return (this.getAttribute('readonly')=='readonly');
+    },
+    set readOnly(value){
+        this.setAttribute('readonly', (value ? 'readonly' :''));
+    },
+    get tabIndex(){
+        return Number(this.getAttribute('tabindex'));
+    },
+    set tabIndex(value){
+        this.setAttribute('tabindex',Number(value));
+    },
+    get type(){
+        return this.getAttribute('type');
+    },
+    set type(value){
+        this.setAttribute('type',value);
+    },
+    get value(){
+        return this.getAttribute('value');
+    },
+    set value(value){
+        this.setAttribute('value',value);
+    },
+    blur:function(){
+        __blur__(this);
+
+        if (this._oldValue != this.getAttribute('value')){
+            var event = document.createEvent();
+            event.initEvent("change");
+            this.dispatchEvent( event );
+        }
+    },
+    focus:function(){
+        __focus__(this);
+        this._oldValue = this.getAttribute('value');
+    },
+    select:function(){
+        __select__(this);
+
+    },
+    click:function(){
+        __click__(this);
+
+    },
+    onchange: function(event){
+        __eval__(this.getAttribute('onchange')||'', this)
+    }
 });
 
 $w.HTMLTextAreaElement = HTMLTextAreaElement;
