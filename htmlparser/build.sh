a2f5c10da04a71a798eaaa9e36dbb34de211a02b
thatcher <thatcher@christopher-thatchers-macbook-air.local>
2009-11-12 08:52:56 -0500

    found error in copy step of parser build, so only min version was being copied correctly

--- htmlparser/build.sh
+++ htmlparser/build.sh
@@ -19,7 +19,7 @@ perl -pi~ -e "s/function kb\(\)/\/\*envjsedit\*\/var kb = Html5Parser = function
 
 echo "Generating Pretty HTMLParser"
 java -XstartOnFirstThread -Xmx256M -cp "$APPDIR/src:$APPDIR/gwt-src:$APPDIR/super:./gwt-1.5.1/gwt-user.jar:./gwt-1.5.1/gwt-dev.jar" com.google.gwt.dev.GWTCompiler -style PRETTY -out "build/pretty" "$@" nu.validator.htmlparser.HtmlParser;
-cp ./build/min/nu.*.HtmlParser/nu.*.HtmlParser.nocache.js ./build/html5.pretty.js
+cp ./build/pretty/nu.*.HtmlParser/nu.*.HtmlParser.nocache.js ./build/html5.pretty.js
 perl -pi~ -e "s/if(.*)\((.*)doc_0\.addEventListener\)/\/\*envjsedit/s" ./build/html5.pretty.js 
 perl -pi~ -e "s/,\s50\);/envjsedit\*\//s" ./build/html5.pretty.js 
 perl -pi~ -e "s/if(.*)\((.*)doc_0\.removeEventListener\)/if\(false\/\*envjsedit\*\/\)/" ./build/html5.pretty.js
@@ -27,7 +27,7 @@ perl -pi~ -e "s/function onBodyDone\(\)/\/\*envjsedit\*\/var onBodyDone = Html5P
 
 echo "Generating Detailed HTMLParser"
 java -XstartOnFirstThread -Xmx256M -cp "$APPDIR/src:$APPDIR/gwt-src:$APPDIR/super:./gwt-1.5.1/gwt-user.jar:./gwt-1.5.1/gwt-dev.jar" com.google.gwt.dev.GWTCompiler -style DETAILED -out "build/detailed" "$@" nu.validator.htmlparser.HtmlParser;
-cp ./build/min/nu.*.HtmlParser/nu.*.HtmlParser.nocache.js ./build/html5.detailed.js
+cp ./build/detailed/nu.*.HtmlParser/nu.*.HtmlParser.nocache.js ./build/html5.detailed.js
 perl -pi~ -e "s/if(.*)\((.*)doc\.addEventListener\)/\/\*envjsedit/s" ./build/html5.detailed.js 
 perl -pi~ -e "s/,\s50\);/envjsedit\*\//s" ./build/html5.detailed.js 
 perl -pi~ -e "s/if(.*)\((.*)doc\.removeEventListener\)/if\(false\/\*envjsedit\*\/\)/" ./build/html5.detailed.js
