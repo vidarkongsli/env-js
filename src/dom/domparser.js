cb2f6c5bc3ffaad09f60a9728cee2edf9e2e7186
Nick <nickg@client9.com>
2010-03-27 10:34:15 -0400

    Ticket 135 -- replace "for each" with equiv with v8.  passes tests

--- src/dom/domparser.js
+++ src/dom/domparser.js
@@ -6,35 +6,37 @@ __extend__(DOMParser.prototype,{
     parseFromString: function(xmlstring, mimetype){
         var doc = new Document(new DOMImplementation()),
             e4;
-        
+
         XML.ignoreComments = false;
         XML.ignoreProcessingInstructions = false;
         XML.ignoreWhitespace = false;
-        
+
         xmlstring = xmlstring.replace(/<\?xml.*\?>/);
-        
+
         e4 = new XMLList(xmlstring);
-        
+
         __toDomNode__(e4, doc, doc);
-        
+
         //console.log('xml \n %s', doc.documentElement.xml);
         return doc;
-        
+
     }
 });
 
 var __toDomNode__ = function(e4, parent, doc){
-    var xnode, 
+    var xnode,
         domnode,
         children,
         target,
         value,
         length,
         element,
-        kind;
+        kind,
+        item;
     //console.log('converting e4x node list \n %s', e4)
-    for each(xnode in e4){
-        kind = xnode.nodeKind(); 
+    for (item in e4) {
+        xnode = e4[item];
+        kind = xnode.nodeKind();
         //console.log('treating node kind %s', kind);
         switch(kind){
             case 'element':
@@ -54,17 +56,17 @@ var __toDomNode__ = function(e4, parent, doc){
                 }
                 break;
             case 'attribute':
-                //console.log('setting attribute %s %s %s', 
+                //console.log('setting attribute %s %s %s',
                 //    xnode.localName(), xnode.namespace(), xnode.text());
                 if(xnode.namespace() && xnode.namespace().prefix){
                     //console.log("%s", xnode.namespace().prefix);
-                    parent.setAttributeNS(xnode.namespace()+'', 
-                        xnode.namespace().prefix+':'+xnode.localName(), 
+                    parent.setAttributeNS(xnode.namespace()+'',
+                        xnode.namespace().prefix+':'+xnode.localName(),
                         xnode.text());
                 }else if((xnode.name()+'').match("http://www.w3.org/2000/xmlns/::")){
                     if(xnode.localName()!=='xmlns'){
-                        parent.setAttributeNS('http://www.w3.org/2000/xmlns/', 
-                            'xmlns:'+xnode.localName(), 
+                        parent.setAttributeNS('http://www.w3.org/2000/xmlns/',
+                            'xmlns:'+xnode.localName(),
                             xnode.text());
                     }
                 }else{
@@ -93,4 +95,4 @@ var __toDomNode__ = function(e4, parent, doc){
                 break;
         }
     }
-}; 
+};
