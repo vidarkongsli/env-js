e9aafd9a5e4b5e5073aa5bba26fef6980f650a0d
Nick <nickg@client9.com>
2010-03-30 01:07:54 -0400

    fix bug where trim was undefined

--- src/css/stylesheet.js
+++ src/css/stylesheet.js
@@ -1,38 +1,40 @@
 
-/* 
-* CSSStyleSheet - DOM Level 2
-*/
+/*
+ * CSSStyleSheet - DOM Level 2
+ */
 CSSStyleSheet = function(options){
-    var $cssRules, 
+    var $cssRules,
         $disabled = options.disabled?options.disabled:false,
         $href = options.href?options.href:null,
         $parentStyleSheet = options.parentStyleSheet?options.parentStyleSheet:null,
         $title = options.title?options.title:"",
         $type = "text/css";
-        
+
     function parseStyleSheet(text){
         //$debug("parsing css");
         //this is pretty ugly, but text is the entire text of a stylesheet
         var cssRules = [];
-        if (!text) text = "";
-        text = trim(text.replace(/\/\*(\r|\n|.)*\*\//g,""));
+        if (!text) {
+            text = "";
+        }
+        text = __trim__(text.replace(/\/\*(\r|\n|.)*\*\//g,""));
         // TODO: @import ?
         var blocks = text.split("}");
         blocks.pop();
-        var i, len = blocks.length;
+        var i, j, len = blocks.length;
         var definition_block, properties, selectors;
         for (i=0; i<len; i++){
-                definition_block = blocks[i].split("{");
-                if(definition_block.length === 2){
-                        selectors = definition_block[0].split(",");
-                        for(var j=0;j<selectors.length;j++){
-                            cssRules.push(new CSSRule({
-                                selectorText:selectors[j],
-                                cssText:definition_block[1]
-                            }));
-                        }
-                        __setArray__($cssRules, cssRules);
+            definition_block = blocks[i].split("{");
+            if(definition_block.length === 2){
+                selectors = definition_block[0].split(",");
+                for(j=0;j<selectors.length;j++){
+                    cssRules.push(new CSSRule({
+                        selectorText:selectors[j],
+                        cssText:definition_block[1]
+                    }));
                 }
+                __setArray__($cssRules, cssRules);
+            }
         }
     };
     parseStyleSheet(options.text);
@@ -64,5 +66,3 @@ CSSStyleSheet = function(options){
         }
     });
 };
-
-
