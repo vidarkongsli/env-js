91f67653e57ccbf899e94a2be490273599dd9fc2
Nick Galbreath <nickg@client9.com>
2010-04-05 23:54:26 -0400

    round 1 of parser update. Not quite done, but checking-in since Im paranoid

---
+++ htmlparser/gwt2/build.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+#
+# Autodownload Parser
+#
+#
+PARSER=htmlparser-1.2.1
+PARSER_ZIP="${PARSER}.zip"
+
+if [ ! -d "${PARSER}" ]; then
+    wget "http://about.validator.nu/htmlparser/${PARSER_ZIP}"
+    unzip ${PARSER_ZIP}
+fi
+
+#
+# Autodownload GWT
+#
+#
+GWT=gwt-2.0.3
+GWT_ZIP="${GWT}.zip"
+
+if [ ! -d "${GWT}" ]; then 
+    wget "http://google-web-toolkit.googlecode.com/files/${GWT_ZIP}"
+    unzip ${GWT_ZIP}
+fi
+
+echo "Starting get compile..."
+CP="./src:./${PARSER}/src:./${PARSER}/super:./${GWT}/gwt-user.jar:./${GWT}/gwt-dev.jar"
+
+# Compile a new GWT linker.  Very simple to Single Script Linker but
+# removes all the "bootstrap" and client (real browser) onScriptLoad
+# events, etc.
+javac -cp ${CP} src/com/envjs/gwt/linker/ServerSingleScriptLinker.java
+
+java \
+    -Xmx256M \
+    -cp "${CP}" \
+    com.google.gwt.dev.Compiler \
+    -logLevel ERROR \
+    -style PRETTY \
+    -draftCompile \
+    nu.validator.htmlparser.HtmlParser;
+
+echo "DONE"
+
