9fe2d3dc9a6d99d7ec1e9670827775f9253936e2
Glen E. Ivey <gleneivey@wontology.org>
2010-04-15 19:34:14 -0700

    Cleaning up and normalizing "boot.js" -- in particular, use concat'ed env.js (actually env.rhino.js) instead of loading individual components like other unit tests do.

--- specs/frame/boot.js
+++ specs/frame/boot.js
@@ -2,23 +2,15 @@
 /**
  * @author thatcher
  */
-load('dist/platform/core.js');
-load('dist/platform/rhino.js');
-load('dist/console.js');
-load('dist/dom.js');
-load('dist/event.js');
-load('dist/html.js');
-load('dist/timer.js');
-load('dist/parser.js');
-load('dist/xhr.js');
-load('dist/css.js');
-load('dist/window.js');
+load('dist/env.rhino.js');
 
 load('specs/qunit.js');
 load('specs/env.qunit.js');
 load('specs/helpers.js');
+
 load('local_settings.js');
+// "load('specs/frame/spec.js');" is in the "index.html" file
 
 
-location =  'specs/frame/index.html';
+location = 'specs/frame/index.html';
 Envjs.wait();
