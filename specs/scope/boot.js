479f621a9f7bd19bdccc80ca628b873045b29f8a
Glen E. Ivey <gleneivey@wontology.org>
2010-04-17 10:06:34 -0700

    Blank framework for porting old scope tests out of "obsolete". (Note that adding a new test now requies matching changes both to build.xml and specs/frame/index.html.)

---
+++ specs/scope/boot.js
@@ -0,0 +1,15 @@
+
+/**
+ * @author thatcher
+ */
+load('dist/env.rhino.js');
+
+load('specs/qunit.js');
+load('specs/env.qunit.js');
+load('specs/helpers.js');
+
+load('local_settings.js');
+// "load('specs/scope/spec.js');" is in the "index.html" file
+
+
+location = 'specs/scope/index.html';
---
+++ specs/scope/spec.js
@@ -0,0 +1,7 @@
+
+QUnit.module('scope');
+
+test("Initial place-holding do-nothing test", function(){
+    ok( true, 'True is true' );
+});
+
