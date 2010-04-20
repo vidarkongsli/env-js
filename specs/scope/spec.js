9ddd0138caa3bde64a5b87acd24fdcbe1b86b8d4
Glen E. Ivey <gleneivey@wontology.org>
2010-04-20 10:58:54 -0700

    Revised iframe-behaviors spec to run in browser.  Ported fixtures for nexted-iframe testing, don't have most asserts yet.

--- specs/scope/spec.js
+++ specs/scope/spec.js
@@ -1,6 +1,7 @@
 
 QUnit.module('scope');
 
+
 test("Basic window object/global scope identity", function(){
 
     fred = "This is a known string with a sentence in it.";
@@ -25,6 +26,7 @@ test("Basic window object/global scope identity", function(){
         '"var" variables defined in external files are window members' );
 });
 
+
 asyncTest("Basic iframe behaviors", function(){
     var adjustExpect = runningUnderEnvjs() ? 0 : -3;
 /* should be this
@@ -45,15 +47,15 @@ asyncTest("Basic iframe behaviors", function(){
     // test dynamic loading of an empty iframe
     var emptyIFrame = document.getElementById('empty-iframe');
     emptyIFrame.onload = function(){
-	ok_accessToIFrame1x(emptyIFrame, contentOfIFrameA,
-	    'empty iframe loads on .src=');
+        ok_accessToIFrame1x(emptyIFrame, contentOfIFrameA,
+            'empty iframe loads on .src=');
     };
     emptyIFrame.src = "../fixtures/scope/iframe1a.html";
 
     // test dynamic reloading of an already-populated iframe
     iframe.onload = function(){
-	ok_accessToIFrame1x(iframe, contentOfIFrameB,
-	    'iframe reloads on .src=');
+        ok_accessToIFrame1x(iframe, contentOfIFrameB,
+            'iframe reloads on .src=');
     }
     iframe.src = "../fixtures/scope/iframe1b.html";
 
@@ -82,7 +84,7 @@ contentOfIFrameB = {
 
 
 // add 7-or-8 to your expect() call's argument for each call to this function
-function ok_accessToIFrame1x(iframe, contentOf, message) {
+function ok_accessToIFrame1x(iframe, contentOf, message){
     ok( iframe.src.match(contentOf.urlRE),
         message + ": Initial iframe src matches test page source" );
 
@@ -98,20 +100,78 @@ function ok_accessToIFrame1x(iframe, contentOf, message) {
 
 
     if (window.top.allTestsAreBeingRunWithinAnExtraIFrame)
-	equals( iframe.contentWindow.top, window.parent, message +
-	    ": '.top' from iframe does point to top window" );
+        equals( iframe.contentWindow.top, window.parent, message +
+            ": '.top' from iframe does point to top window" );
     else
-	equals( iframe.contentWindow.top, window, message +
-	    ": '.top' from iframe does point to top window" );
+        equals( iframe.contentWindow.top, window, message +
+            ": '.top' from iframe does point to top window" );
 
 
     // document.parentWindow is IE-specific extension implemented by env.js
     if (runningUnderEnvjs()){
-	equals( idoc.parentWindow, iframe.contentWindow, message +
-	    ": iframe doc's .parentWindow points to iframe's .contentWindow");
+        equals( idoc.parentWindow, iframe.contentWindow, message +
+            ": iframe doc's .parentWindow points to iframe's .contentWindow");
 /* re-enable this once the preceding passes
-	equals( idoc.parentWindow.parent, window, message +
-	    ": Can follow chain from iframe's doc to containing window");
+        equals( idoc.parentWindow.parent, window, message +
+            ": Can follow chain from iframe's doc to containing window");
 */
     }
-};
+}
+
+
+asyncTest("Iframe nesting", function(){
+    var startingDepth = 3;
+    var endingDepth   = 7;
+
+    expect(4);
+// + (10*((endingDepth - startingDepth)+1)));
+
+    window.numberNestedIframeLoads = 2;
+    window.windowLoadCount = 0;
+
+    var topNestingIFrame = document.getElementById('nesting-iframe');
+    topNestingIFrame.onload = function(){
+// iframe2.html contains a static <iframe> element that loads iframe3.html,
+// now we should have both loaded, with the structure that
+//     index.html --contains--> iframe2.html --contains--> iframe3.html
+// w/ id's =     nesting-iframe              nested1Level
+
+        // verify we have as described above
+        ok( topNestingIFrame.contentDocument.title.match(/nested-IFRAME/),
+            "top-level IFRAME loaded from correct source" );
+        var iframeNested1 = topNestingIFrame.contentDocument.
+            getElementById('nested1Level');
+        ok( iframeNested1.contentDocument.title.match(/IFRAME loading/),
+            "can access content of one IFRAME nested in another" );
+        equals( iframeNested1.contentWindow.parent.parent, window,
+            "can follow 'parent' path from nested IFRAME to root window");
+        equals( iframeNested1.contentWindow.top, window,
+            "nested IFRAME has correct .top");
+
+        // now, we'll programatically extend the nesting depth from 2 to many
+        recursivelyInsertIFrames(startingDepth, endingDepth, iframeNested1);
+    };
+    topNestingIFrame.src = "../fixtures/scope/iframe2.html";
+
+    setTimeout(function(){
+        start();
+    }, 1000);
+});
+
+
+function recursivelyInsertIFrames(depth, finalDepth, existingIframe){
+    var newIframe;
+    newIframe = existingIframe.contentDocument.createElement("iframe");
+    newIframe.setAttribute("id", "iframe_of_depth_" + depth);
+    newIframe.setAttribute("style", "border: 3px solid blue; padding: 1em; " +
+        "width: 95%; height: " + (1100-(140*depth)) + "px;");
+    newIframe.src = "/env-js/specs/fixtures/scope/iframe" + (depth+1) + ".html";
+
+    if (depth < finalDepth)
+        newIframe.onload = function(){
+            recursivelyInsertIFrames(depth+1, finalDepth, newIframe);
+        };
+
+    existingIframe.contentDocument.getElementsByTagName('body')[0].
+        appendChild(newIframe);
+}
