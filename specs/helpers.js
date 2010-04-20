26bad612922ead09b19803258774a09858c34c5b
Glen E. Ivey <gleneivey@wontology.org>
2010-04-20 11:33:53 -0700

    Had forgotten copyright headers in files I'd added recently.  Fixed.

--- specs/helpers.js
+++ specs/helpers.js
@@ -1,3 +1,14 @@
+/*
+ *
+ * This file is a component of env.js,
+ *     http://github.com/thatcher/env-js/
+ * a Pure JavaScript Browser Environment
+ * Copyright 2010 John Resig, licensed under the MIT License
+ *     http://www.opensource.org/licenses/mit-license.php
+ *
+ */
+
+
 // helpers available to all tests
 
 function runningUnderEnvjs(){
--- specs/scope/boot.js
+++ specs/scope/boot.js
@@ -1,7 +1,14 @@
-
 /**
+ *
+ * This file is a component of env.js,
+ *     http://github.com/thatcher/env-js/
+ * a Pure JavaScript Browser Environment
+ * Copyright 2010 John Resig, licensed under the MIT License
+ *     http://www.opensource.org/licenses/mit-license.php
+ *
  * @author thatcher
  */
+
 load('dist/env.rhino.js');
 
 load('specs/qunit.js');
